05e379c394533c59e89965b2959ee30c2d0517e3
Eslam030 <93558896+Eslam030@users.noreply.github.com>
2022-10-27 22:50:38 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,9 @@
 const content = document.querySelector('#contain').children;
 const list = []
-/*document.addEventListener("click", function () {
-    scrollTo(0, 0);
-    console.log("fdfd")
-})*/
 document.querySelector('html').style.scrollBehavior = "smooth"
 for (let i = 0; i < content.length; i++) {
     list.push(content[i].firstElementChild.textContent);
+    content[i].style.cssText = "padding : 55px"
 }
 const ul = document.createElement('ul');
 ul.style.columnGap = "30px"
@@ -15,6 +12,15 @@ let y = -50;
 for (let i = 0; i < list.length; i++) {
     const li = document.createElement('li');
     const an = document.createElement('a');
+    an.style.cssText = "padding : 9px";
+    an.addEventListener("mouseover", function (event) {
+        event.target.parentElement.style.backgroundColor = "green";
+        event.target.parentElement.style.border = "2px solid green";
+    })
+    an.addEventListener("mouseout", function (event) {
+        event.target.parentElement.style.backgroundColor = "grey";
+        event.target.parentElement.style.border = "2px solid grey";
+    })
     an.textContent = list[i];
     li.appendChild(an);
     li.style.cssText = "margin-top : 5px ; font-weight : bold ; border : 2px solid grey ; padding-bottom : 5px ; background-color : grey ; color : red ;  box-shadow: 2px 2px 2px 2px lightgreen; cursor : pointer"
@@ -29,15 +35,7 @@ for (let i = 0; i < list.length; i++) {
     li.addEventListener('click', function (event) {
         const element = document.querySelector(event.target.parentElement.id);
         event.preventDefault();
-        console.log((element.getBoundingClientRect().y))
-        scrollTo(0, Math.round(element.getBoundingClientRect().y) + y);
-        if (Math.round(scrollY + innerHeight) + 4 != document.body.offsetHeight) {
-            y += Math.round(element.getBoundingClientRect().y) - 50;
-        }
-        console.log((element.getBoundingClientRect().y))
-        console.log(element.offsetHeight)
-        console.log(y);
-
+        element.scrollIntoView(true);
     })
     ul.appendChild(li)
 }
